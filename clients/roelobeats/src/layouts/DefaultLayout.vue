9155d4969da79331d593f79f71d5ad9aff3c89e9
roelobeats <raul.santing@gmail.com>
2023-01-20 15:00:40 +0100

    feat(roelobeats): added footer & header

---
+++ clients/roelobeats/src/layouts/DefaultLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <AppHeader />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <AppFooter />
+  </div>
+</template>
+<script>
+import AppFooter from '@/components/AppFooter.vue';
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  components: { AppFooter, AppHeader },
+};
+</script>
