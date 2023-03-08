588883079c51205e1b7a02b0045114fd732058ff
Martijn <martijn.brug@gmail.com>
2023-03-08 08:57:13 +0100

    feat(cryptherion): seo fixes

--- clients/cryptherion/content.queries.js
+++ clients/cryptherion/content.queries.js
@@ -11,6 +11,25 @@ const GET_CONTENT = gql`
       highlighted_product
       banner
     }
+    cryptherion_blogs {
+      id
+      date_created
+      user_created {
+        first_name
+        last_name
+        avatar {
+          id
+        }
+      }
+      title
+      slug
+      description
+      featured_image {
+        id
+        title
+      }
+      content
+    }
   }
 `;
 
