e025152ce1eb2b874ba9184258d2df825e351e3e
Martijn <martijn.brug@gmail.com>
2023-03-29 12:11:36 +0200

    feat(wkw): video on 404

--- clients/wkw/src/templates/404.vue
+++ clients/wkw/src/templates/404.vue
@@ -5,7 +5,7 @@
 
       <div class="has-text-centered">
         <video width="320" height="240" autoplay muted loop>
-          <source src="404.mp4" type="video/mp4" />
+          <source src="/404.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
