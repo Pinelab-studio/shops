c6a8189ee46196e42ba0d29f798c07c6e0db3055
Martijn <martijn.brug@gmail.com>
2023-11-08 14:46:44 +0100

    feat(cantastic): blog layout fix

--- clients/cantastic/src/templates/BlogListing.vue
+++ clients/cantastic/src/templates/BlogListing.vue
@@ -3,9 +3,9 @@
     <template #content>
       <section id="blog">
         <h3>Blogs</h3>
-        <div class="columns">
+        <div class="columns is-multiline">
           <template v-for="blog of $context.blogs">
-            <div class="column">
+            <div class="column is-4">
               <BlogCard :blog="blog" />
             </div>
           </template>
