25b5933956c8c21184af27e0e4ce66851aedda4a
Martijn <martijn.brug@gmail.com>
2024-02-20 08:59:49 +0100

    feat(cantastic): cookie consent

--- clients/cantastic/src/App.vue
+++ clients/cantastic/src/App.vue
@@ -5,16 +5,30 @@
     <!--  <router-view />-->
     <Consent
       class="consent pb-6"
-      accept-text="Ja, dat is goed"
+      accept-text="Accepteren"
       decline-text="Nee"
       thank-you-message="Bedankt!"
       v-on:approved="activateAnalytics()"
     >
       <br />
-      <h4>Cookies</h4>
-      Vind je het goed dat we geanonimiseerde data naar Google Analytics sturen,
-      om de website te verbeteren?
-      <br />
+      <h4>Privacy</h4>
+      <small>
+        Door op "Accepteren" te klikken, stemt je ermee in dat we bepaalde
+        informatie verzamelen om:
+        <br />
+        <br />
+        <li>Gebruikersgegevens te gebruiken voor advertentiepersonalisatie</li>
+        <li>Ad-gerelateerde gegevens op uw apparaat op te slaan</li>
+        <li>
+          Analysegegevens te verzamelen en op te slaan om onze diensten te
+          verbeteren
+        </li>
+        <li>
+          Analysegegevens te verzamelen en op te slaan om onze diensten te
+          verbeteren
+        </li>
+        <br />
+      </small>
     </Consent>
   </div>
 </template>
@@ -38,6 +52,16 @@ export default {
   methods: {
     async activateAnalytics() {
       this.$gtm.enable(true);
+      // Consent parameters: https://developers.google.com/tag-platform/security/guides/consent?consentmode=basic
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('consent', 'update', {
+        ad_user_data: 'granted',
+        ad_personalization: 'granted',
+        ad_storage: 'granted',
+        analytics_storage: 'granted',
+      });
     },
   },
   metaInfo() {
