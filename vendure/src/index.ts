e0b6419320e511ee057c1bb80d934282f7624756
Martijn <martijn.brug@gmail.com>
2024-05-10 09:26:06 +0200

    fix(vendure): request context patch

--- vendure/src/index.ts
+++ vendure/src/index.ts
@@ -1,5 +1,10 @@
 require('dotenv').config({ path: process.env.ENV_FILE });
-import { bootstrap, JobQueueService, Logger } from '@vendure/core';
+import {
+  bootstrap,
+  JobQueueService,
+  Logger,
+  RequestContextCacheService,
+} from '@vendure/core';
 import { INestApplication } from '@nestjs/common';
 import { config, runningInWorker, runningLocal } from './vendure-config';
 
@@ -14,6 +19,24 @@ bootstrap(config)
     }
     Logger.info(`Bootstrapped Vendure for env ${process.env.SHOP_ENV}`);
     app = _app;
+
+    // FIXME: Dirty hack to avoid caching of wrong 'activeTaxZone'. This patch makes sure get() never returns anything for activeTaxZone
+    // This is due to a wrong cache setting in Vendure core. The commit below (v2.2.0) should fix this issue:
+    // https://github.com/vendure-ecommerce/vendure/commit/e543e5e7006140518be311bc7e60687bd9b40778
+    // Next block should be removed when upgraded to v2.2.0
+    const requestContextCache = app.get(RequestContextCacheService);
+    const originalGet = requestContextCache.get;
+    requestContextCache.get = function (ctx, key, getDefault?: any) {
+      if (key === 'activeTaxZone') {
+        return getDefault?.();
+      } else {
+        return originalGet.call(requestContextCache, ctx, key, getDefault);
+      }
+    };
+    Logger.error(
+      `FIXME: remove this after v2.2.0: Patched RequestContextCacheService.get() to avoid caching of wrong 'activeTaxZone'`
+    );
+    // End of ugly patch block
   })
   .catch((err) => {
     console.error(err);
