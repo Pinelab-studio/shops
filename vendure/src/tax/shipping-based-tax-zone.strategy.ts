9b986167ba9743371492a05763518e532c70092e
Martijn <martijn.brug@gmail.com>
2024-05-10 08:41:14 +0200

    fix(vendure): reverted change

--- vendure/src/tax/shipping-based-tax-zone.strategy.ts
+++ vendure/src/tax/shipping-based-tax-zone.strategy.ts
@@ -22,15 +22,15 @@ export class ShippingBasedTaxZoneStrategy implements TaxZoneStrategy {
     // This is due to a wrong cache setting in Vendure core. The commit below (v2.2.0) should fix this issue:
     // https://github.com/vendure-ecommerce/vendure/commit/e543e5e7006140518be311bc7e60687bd9b40778
     // Next block should be removed when upgraded to v2.2.0
-    const requestContextCache = app.get(RequestContextCacheService);
-    const originalGet = requestContextCache.get;
-    requestContextCache.get = function (ctx, key, getDefault?: any) {
-      if (key === 'activeTaxZone') {
-        return getDefault?.();
-      } else {
-        return originalGet.call(requestContextCache, ctx, key, getDefault);
-      }
-    }; // End of ugly patch block
+    // const requestContextCache = app.get(RequestContextCacheService);
+    // const originalGet = requestContextCache.get;
+    // requestContextCache.get = function (ctx, key, getDefault?: any) {
+    //   if (key === 'activeTaxZone') {
+    //     return getDefault?.();
+    //   } else {
+    //     return originalGet.call(requestContextCache, ctx, key, getDefault);
+    //   }
+    // }; // End of ugly patch block
 
     const countryCode = order?.shippingAddress?.countryCode;
     if (order && countryCode) {
